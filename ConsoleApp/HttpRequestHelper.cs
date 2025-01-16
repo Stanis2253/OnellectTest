b9690a40d2e81b798098a1ff3a71f9c062b1d1e2
Станислав Саакян <stanis2253@gmail.com>
2025-01-16 14:33:11 +0300

    feat

---
+++ ConsoleApp/HttpRequestHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class HttpRequestHelper
+    {
+        public static async Task GetRequest(string json, string url)
+        {
+            using HttpClient client = new HttpClient();
+
+            // Упаковка данных в тело запроса
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            // Отправка POST-запроса
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            // Обработка ответа
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Response: {responseBody}");
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using NumberGenerator;
+using Microsoft.Extensions.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.Json;
+using System.Net;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        string baseUrl = configuration["ApiSettings:BaseUrl"];
+
+        Do(baseUrl);
+    }
+
+    private static void Do(string BaseUrl)
+    {
+        GeneratorNums generator = new GeneratorNums();
+
+        (List<int> GenList, List<int> SortList) = generator.Generate();
+
+        Console.WriteLine("Список случайных чисел");
+
+        foreach (var item in GenList)
+            Console.Write(item + " ");
+
+        Console.WriteLine();
+        Console.WriteLine("Список отсортированных случайных чисел");
+
+        foreach (var item in SortList)
+            Console.Write(item + " ");
+
+        string json = JsonSerializer.Serialize(SortList);
+
+        ConsoleApp.HttpRequestHelper.GetRequest(json, BaseUrl);
+
+        Console.ReadKey();
+    }
+
+}
